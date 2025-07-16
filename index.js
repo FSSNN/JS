8b795769273b92ef69de070c2ac8cce86053822a
FSSNN <Felipe-sack@hotmail.com>
2025-07-16 07:32:03 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
+
+
+
+const contaCorrenteRicardo = new ContaCorrente(101, cliente1);
+
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente(102, cliente2);
+
+conta2.depositar(300);
+
+let valor = 200
+contaCorrenteRicardo.transferir(200, conta2);
+
+console.log(ContaCorrente.numeroDeContas);
